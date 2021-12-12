34d7511851c81243b8d794f2389a2a23759fdcbe
Miguel Silva <miguelsilv98@gmail.com>
2021-12-12 18:00:10 -0300

    Casos de testes

---
+++ src/core/domain/repositories/exam/exam.repository.spec.ts
@@ -0,0 +1,23 @@
+describe("ExamRepository", () => {
+
+    beforeEach(() => {
+
+    });
+
+    describe("Cadastrar um novo exame", () => {
+
+    });
+
+    describe("Obter uma lista de exames ativos", () => {
+
+    });
+
+    describe("Atualizar um exame existente", () => {
+
+    });
+
+    describe("Remover logicamente um exame ativo", () => {
+
+    });
+
+});
---
+++ src/core/domain/repositories/laboratory/laboratory.repository.spec.ts
@@ -0,0 +1,23 @@
+describe("ExamRepository", () => {
+
+    beforeEach(() => {
+
+    });
+
+    describe("Cadastrar um novo laboratório", () => {
+
+    });
+
+    describe("Obter uma lista de laboratórios ativos", () => {
+
+    });
+
+    describe("Atualizar um laboratório existente", () => {
+
+    });
+
+    describe("Remover logicamente um laboratório ativo", () => {
+
+    });
+
+});
