ba318ce50996e9fd16789a78a42882fb776d4e64
Miguel Silva <miguelsilv98@gmail.com>
2021-12-15 00:49:57 -0300

    Usando repositories do banco de dados

--- src/presentation/exam/exam.module.ts
+++ src/presentation/exam/exam.module.ts
@@ -6,6 +6,7 @@ import { CreateExamUseCase } from 'src/core/use-cases/exam/create-exam.usecase';
 import { GetAllExamsUseCase } from 'src/core/use-cases/exam/get-all-exams.usecase';
 import { UpdateExamUseCase } from 'src/core/use-cases/exam/update-exam.usecase';
 import { DeleteExamUseCase } from 'src/core/use-cases/exam/delete-exam.usecase';
+import { ExamDataRepository } from 'src/data/exam/exam-data.repository';
 
 @Module({
     controllers: [
@@ -16,7 +17,7 @@ import { DeleteExamUseCase } from 'src/core/use-cases/exam/delete-exam.usecase';
         GetAllExamsUseCase,
         UpdateExamUseCase,
         DeleteExamUseCase,
-        { provide: ExamRepository, useClass: ExamMockRepository, }
+        { provide: ExamRepository, useClass: ExamDataRepository, }
     ]
 })
 export class ExamModule { }
--- src/presentation/laboratory/laboratory.module.ts
+++ src/presentation/laboratory/laboratory.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common';
-import { LaboratoryMockRepository } from 'src/data/laboratory/laboratory-mock.repository';
 import { LaboratoryRepository } from 'src/core/domain/repositories/laboratory/laboratory.repository';
 import { LaboratoryController } from './laboratory.controller';
 import { CreateLaboratoryUseCase } from 'src/core/use-cases/laboratory/create-laboratory.usecase';
 import { UpdateLaboratoryUseCase } from 'src/core/use-cases/laboratory/update-laboratory.usecase';
 import { DeleteLaboratoryUseCase } from 'src/core/use-cases/laboratory/delete-laboratory.usecase';
 import { GetAllLaboratoriesUseCase } from 'src/core/use-cases/laboratory/get-all-laboratories.usecase';
+import { LaboratoryDataRepository } from 'src/data/laboratory/laboratory-data.repository';
 
 @Module({
     controllers: [
@@ -16,7 +16,7 @@ import { GetAllLaboratoriesUseCase } from 'src/core/use-cases/laboratory/get-all
         GetAllLaboratoriesUseCase,
         UpdateLaboratoryUseCase,
         DeleteLaboratoryUseCase,
-        { provide: LaboratoryRepository, useClass: LaboratoryMockRepository, }
+        { provide: LaboratoryRepository, useClass: LaboratoryDataRepository, }
     ]
 })
 export class LaboratoryModule { }
